3be26f5d3ab7b0b24cadb0b27c86d59ebb417cdf
Doug Linley <doug.linley@gmail.com>
2024-02-06 23:20:35 -0500

    Initial commit. Just playing around with monogame

---
+++ Blast/Game1.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Blast
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Texture2D _playerTexture;
+        // private Texture2D _goodProjectile;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            _playerTexture = Content.Load<Texture2D>("player");
+            // _goodProjectile = Content.Load<Texture2D>("good_projectile");
+           
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+            
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(_playerTexture, new Vector2(150, 150), Color.White);
+
+            _spriteBatch.End();
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Blast/Sprite.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace Blast
+{
+    public class Sprite
+    {
+        public Sprite(Texture2D texture)
+        {
+            if (texture == null)
+                throw new Exception("Texture was not expected to be null.");
+
+            Texture = texture;
+            Scale = Vector2.One;
+            Size = new Vector2(Texture.Width, Texture.Height);
+            Color = Color.White;
+            Rotation = 0f;
+        }
+
+        public Sprite(Texture2D texture, Vector2 scale, Color color, float rotation)
+        {
+            if (texture == null)
+                throw new Exception("Texture was not expected to be null.");
+
+            Texture = texture;
+            Scale = scale;
+            Size = new Vector2(Texture.Width, Texture.Height);
+            Color = color;
+            Rotation = rotation;
+        }
+
+        public Texture2D Texture { get; private set; }
+        
+        public Vector2 Size { get; private set; }
+
+        public Vector2 Scale { get; set; }
+
+        public Color Color { get; set; }
+
+        public float Rotation { get; set; }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 position)
+        {
+            spriteBatch.Draw(
+                Texture,
+                position,
+                null,
+                Color,
+                0f,
+                new Vector2(Size.X / 2, Size.Y / 2),
+                Scale,
+                SpriteEffects.None,
+                0f);
+        }
+    }
+}
